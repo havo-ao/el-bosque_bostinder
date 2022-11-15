af6d280855dc1469e0c2869b44f4f300e7d62a5f
Andres <acanon@hellobuild.co>
2022-11-15 04:17:51 -0500

    first commit - basic MVC

---
+++ src/co/edu/unbosque/controllers/Controller.java
@@ -0,0 +1,27 @@
+package co.edu.unbosque.controllers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+public class Controller implements ActionListener, ItemListener {
+
+	public void start() {
+		addListeners();
+	};
+
+	private void addListeners() {
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent actionEvent) {
+
+	}
+
+	@Override
+	public void itemStateChanged(ItemEvent itemEvent) {
+
+	}
+}
---
+++ src/co/edu/unbosque/controllers/MainApp.java
@@ -0,0 +1,45 @@
+package co.edu.unbosque.controllers;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		new MainApp().initialize();
+	}
+
+	public void initialize() {
+		initializeViews();
+		initializeModels();
+		initializeControllers();
+
+		setControllerToViews();
+		setControllerToModels();
+
+		setViewsToController();
+		setModelsToController();
+
+	}
+
+	private void initializeViews() {
+	}
+
+	private void initializeModels() {
+	}
+
+	private void initializeControllers() {
+	}
+
+	private void setControllerToViews() {
+	}
+
+	private void setControllerToModels() {
+
+	}
+
+	private void setViewsToController() {
+	}
+
+	private void setModelsToController() {
+
+	}
+
+}
